19510f5baefd22534f7b9078c4b11aeb25f20c49
Amelia0208 <gy20arw@leeds.ac.uk>
2021-05-24 21:06:58 +0100

    Adding in files

---
+++ model.py
@@ -0,0 +1,22 @@
+import csv
+
+lidar = []
+radar = []
+
+f = open('lidar1.txt', newline='')
+dataset = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+for row in dataset:
+    rowlist = []
+    for value in row:
+        rowlist.append(value)
+    lidar.append(rowlist)
+f.close()
+
+f = open('radar1.txt', newline='')
+dataset = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+for row in dataset:
+    rowlist = []
+    for value in row:
+        rowlist.append(value)
+    radar.append(rowlist)
+f.close()
