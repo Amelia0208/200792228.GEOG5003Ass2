c099b4ccbb292150200185d9b9dd98ba838b4fc2
Amelia0208 <gy20arw@leeds.ac.uk>
2021-05-27 23:58:43 +0100

    Set up model with defs

--- icedetection.py
+++ icedetection.py
@@ -1,54 +1,115 @@
+import matplotlib
 import csv
 
 class Ice:
     # Set variables
-    def __init__(self, lidar_set1, radar_set1, lidar_set2, radar_set2):
-        self.x = [range(299)]
-        self.y = [range(299)]
-        self.lidar_set1 = lidar_set1
-        self.radar_set1 = radar_set1
-        self.lidar_set2 = lidar_set2
-        self.radar_set2 = radar_set2
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.radar_set = []
+        self.lidar_set = []
+        self.vol = 0
+        self.volume = []
+        self.mass = []
+        self.can_pull = []
         
-    def lidar1(self, lidar_set1):
-        f = open('lidar1.txt', newline='')
-        dataset_lidar1 = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-        for row in dataset_lidar1:
-            rowlist_lidar1 = []
-            for value in row:
-                rowlist_lidar1.append(value)
-            lidar_set1.append(rowlist_lidar1)
-        f.close()
-        
-    def radar1(self, radar_set1):
-        radar_set1 = []
-        f = open('radar1.txt', newline='')
-        dataset_radar1 = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-        for row in dataset_radar1:
-            rowlist_radar1 = []
-            for value in row:
-                rowlist_radar1.append(value)
-            radar_set1.append(rowlist_radar1)
-        f.close()
-        
-    def lidar2(self, lidar_set2):
-        lidar_set2 = []
+  
+    def call_1_():
         f = open('lidar2.txt', newline='')
-        dataset_lidar2 = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-        for row in dataset_lidar2:
-            rowlist_lidar2 = []
+        dataset_lidar = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+        for row in dataset_lidar:
+            rowlist_lidar = []
             for value in row:
-                rowlist_lidar2.append(value)
-            lidar_set2.append(rowlist_lidar2)
+                rowlist_lidar.append(value)
+            lidar_set.append(rowlist_lidar)
         f.close()
         
-    def radar2(self, radar_set2):
-        radar_set2 = []
+        
         f = open('radar2.txt', newline='')
-        dataset_radar2 = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-        for row in dataset_radar2:
-            rowlist_radar2 = []
+        dataset_radar = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+        for row in dataset_radar:
+            rowlist_radar = []
             for value in row:
-                rowlist_radar2.append(value)
-            radar_set2.append(rowlist_radar2)
+                rowlist_radar.append(value)
+            radar_set.append(rowlist_radar)
         f.close()
+        
+        
+    def call_2_(self, radar_set, lidar_set):
+        f = open('lidar2.txt', newline='')
+        dataset_lidar = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+        for row in dataset_lidar:
+            rowlist_lidar = []
+            for value in row:
+                rowlist_lidar.append(value)
+            lidar_set.append(rowlist_lidar)
+        f.close()
+        
+        
+        f = open('radar2.txt', newline='')
+        dataset_radar = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+        for row in dataset_radar:
+            rowlist_radar = []
+            for value in row:
+                rowlist_radar.append(value)
+            radar_set.append(rowlist_radar)
+        f.close()
+        
+        
+    def identify_ice():
+        for i in range(299):
+            for j in range(299):
+                if radar_set[i][j] >= 100:
+                    radar_set[i][j] = 1
+                else:
+                    radar_set[i][j] = 0
+                    
+                    
+    def calc_volume():
+        for i in range(1, 298):
+            for j in range(1, 298):
+                if radar_set[i - 1][j] != 0:
+                    pass
+                else: 
+                    if radar_set[i][j - 1] != 0:
+                        pass
+                    else:
+                        if radar_set[i][j] != 0:
+                            for m in range(j, 299):
+                                if radar_set[i][m] == 0:
+                                    break
+                                else:
+                                    for k in range(i, 299):
+                                        if radar_set[k][m] == 0:
+                                            break
+                                        else:
+                                            vol = vol + lidar_set[k][m]
+                            volume.append(vol)
+                            vol = 0
+                            
+                            
+    def calc_mass():
+        for i in range(len(volume)):
+            ice_mass = volume[i] * 900
+            mass.append(ice_mass)
+            
+            
+    def calc_can_pull():
+        for i in range(len(volume)):
+            if mass[i] <=36000000:
+                can_pull_ice = 'yes'
+            else:
+                can_pull_ice = 'no'
+            can_pull.append(can_pull_ice)
+            
+    def print_results():
+        for i in range(len(volume)):
+            print('Iceberg ' + str(i + 1) + ':\nVolume: ' + str(volume[i]) + '\nMass: ' + str(mass[i]) + '\nCan be pulled: ' + str(can_pull[i]) + '\n\n')
+        
+    def plot():
+        matplotlib.pyplot.xlim(0, 299) # Environment needs to be set
+        matplotlib.pyplot.ylim(0, 299)
+        
+        matplotlib.pyplot.imshow(radar_set)
+    
+    
--- model.py
+++ model.py
@@ -1,134 +1,129 @@
 import csv
-import icedetection
 import matplotlib
 
-lidar_set1 = []
-radar_set1 = []
-lidar_set2 = []
-radar_set2 = []
+lidar_set = []
+radar_set = []
+volume = []
+mass = []
+can_pull = []
 
 
-f = open('lidar1.txt', newline='')
-dataset_lidar1 = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-for row in dataset_lidar1:
-    rowlist_lidar1 = []
-    for value in row:
-        rowlist_lidar1.append(value)
-    lidar_set1.append(rowlist_lidar1)
-f.close()
-        
-        
-f = open('radar1.txt', newline='')
-dataset_radar1 = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-for row in dataset_radar1:
-    rowlist_radar1 = []
-    for value in row:
-        rowlist_radar1.append(value)
-    radar_set1.append(rowlist_radar1)
-f.close()
-     
-
-f = open('lidar2.txt', newline='')
-dataset_lidar2 = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-for row in dataset_lidar2:
-    rowlist_lidar2 = []
-    for value in row:
-        rowlist_lidar2.append(value)
-    lidar_set2.append(rowlist_lidar2)
-f.close()
-        
-        
-f = open('radar2.txt', newline='')
-dataset_radar2 = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-for row in dataset_radar2:
-    rowlist_radar2 = []
-    for value in row:
-        rowlist_radar2.append(value)
-    radar_set2.append(rowlist_radar2)
-f.close()
-
-
-def model1():
-    matplotlib.pyplot.xlim(0, 299) # Environment needs to be set
-    matplotlib.pyplot.ylim(0, 299)
-    
-    volume = 0
-    
+def identify_ice():
     for i in range(299):
         for j in range(299):
-            if radar_set1[i][j] >= 100:
-                radar_set1[i][j] = 1
-                volume = volume + lidar_set1[i][j]
+            if radar_set[i][j] >= 100:
+                radar_set[i][j] = 1
             else:
-                radar_set1[i][j] = 0
-                
-    mass = volume * 900
-    
-    if mass <= 36000000:
-        can_pull = 'yes'
-        for i in range(299):
-            for j in range(299):
-                if radar_set1[i][j] == 1:
-                    radar_set1[i][j] = 2
-    else:
-        can_pull = 'no'
-    
-    print('Volume: ' + str(volume) + '\nMass: ' + str(mass) + '\nCan be pulled: ' + str(can_pull))
-    
-    matplotlib.pyplot.imshow(radar_set1)
-    
-def model2():
-    matplotlib.pyplot.xlim(0, 299) # Environment needs to be set
-    matplotlib.pyplot.ylim(0, 299)
-    
+                radar_set[i][j] = 0
+                    
+                    
+def calc_volume():
     vol = 0
-    volume = []
-    mass = []
-    can_pull = []
-    
-    for i in range(299):
-        for j in range(299):
-            if radar_set2[i][j] >= 100:
-                radar_set2[i][j] = 1
-            else:
-                radar_set2[i][j] = 0
-    
     for i in range(1, 298):
         for j in range(1, 298):
-            if radar_set2[i - 1][j] != 0:
+            if radar_set[i - 1][j] != 0:
                 pass
             else: 
-                if radar_set2[i][j - 1] != 0:
+                if radar_set[i][j - 1] != 0:
                     pass
                 else:
-                    if radar_set2[i][j] != 0:
+                    if radar_set[i][j] != 0:
                         for m in range(j, 299):
-                            if radar_set2[i][m] == 0:
+                            if radar_set[i][m] == 0:
                                 break
                             else:
                                 for k in range(i, 299):
-                                    if radar_set2[k][m] == 0:
+                                    if radar_set[k][m] == 0:
                                         break
                                     else:
-                                        vol = vol + lidar_set2[k][m]
+                                        vol = vol + lidar_set[k][m]
                         volume.append(vol)
                         vol = 0
-                        
+                            
+                            
+def calc_mass():
     for i in range(len(volume)):
         ice_mass = volume[i] * 900
         mass.append(ice_mass)
-        
+            
+            
+def calc_can_pull():
     for i in range(len(volume)):
         if mass[i] <=36000000:
             can_pull_ice = 'yes'
         else:
             can_pull_ice = 'no'
         can_pull.append(can_pull_ice)
-                                    
-    print(volume)
-    print(mass)
-    print(can_pull)
+            
+def print_results():
+    for i in range(len(volume)):
+        print('Iceberg ' + str(i + 1) + ':\nVolume: ' + str(volume[i]) + '\nMass: ' + str(mass[i]) + '\nCan be pulled: ' + str(can_pull[i]) + '\n\n')
+        
+def plot():
+    matplotlib.pyplot.xlim(0, 299) # Environment needs to be set
+    matplotlib.pyplot.ylim(0, 299)  
+    matplotlib.pyplot.imshow(radar_set)
+
+
+def model1():    
+    f = open('lidar1.txt', newline='')
+    dataset_lidar = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+    for row in dataset_lidar:
+        rowlist_lidar = []
+        for value in row:
+            rowlist_lidar.append(value)
+        lidar_set.append(rowlist_lidar)
+    f.close()
+        
+        
+    f = open('radar1.txt', newline='')
+    dataset_radar = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+    for row in dataset_radar:
+        rowlist_radar = []
+        for value in row:
+            rowlist_radar.append(value)
+        radar_set.append(rowlist_radar)
+    f.close()
     
-    matplotlib.pyplot.imshow(radar_set2)
+    identify_ice()
+    calc_volume()
+    calc_mass()
+    calc_can_pull()
+    print_results()
+    plot()
+    
+    
+
+
+def model2():
+    matplotlib.pyplot.xlim(0, 299) # Environment needs to be set
+    matplotlib.pyplot.ylim(0, 299)
+    
+    f = open('lidar2.txt', newline='')
+    dataset_lidar = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+    for row in dataset_lidar:
+        rowlist_lidar = []
+        for value in row:
+            rowlist_lidar.append(value)
+        lidar_set.append(rowlist_lidar)
+    f.close()
+        
+        
+    f = open('radar2.txt', newline='')
+    dataset_radar = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+    for row in dataset_radar:
+        rowlist_radar = []
+        for value in row:
+            rowlist_radar.append(value)
+        radar_set.append(rowlist_radar)
+    f.close()
+    
+    identify_ice()
+    calc_volume()
+    calc_mass()
+    calc_can_pull()
+    print_results()
+    plot()
     
 model2()
+    
